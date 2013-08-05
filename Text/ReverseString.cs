e4fc5e47a04fb498725be6c65c940c6d57678057
Luiz <lpicanco@gmail.com>
2013-08-05 10:13:47 -0300

    Reverse String

---
+++ Text/ReverseString.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hundredprojects.Text
+{
+    /// <summary>
+    /// Reverse a String – Enter a string and the program will reverse it and print it out.
+    /// </summary>
+    class ReverseString
+    {
+        static void Main(String[] args)
+        {
+            Console.Write("Enter a text: ");
+            var text = Console.ReadLine();
+
+            var array = text.ToCharArray();
+            Array.Reverse(array);
+            Console.WriteLine("Reversed text: {0}",  new String(array));
+
+            Console.ReadLine();
+        }
+    }
+}
